a16d27668a09d218b0f037356ada41f717457f72
Zach Gulde <zachgulde@gmail.com>
2019-03-30 16:54:36 -0500

    Update package info, add license

---
+++ release.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload dist/*
---
+++ zgulde/contact_info/__main__.py
@@ -0,0 +1,8 @@
+print('Zach Gulde')
+print('')
+print('Email:')
+print('  personal: zach.gulde@gmail.com')
+print('  work:     zach@codeup.com')
+print('')
+print('Website:')
+print('  https://zgul.de')
