e4fff2bc3bcf2bbe1fc31b798aa4496e9b514c45
Zach Gulde <zachgulde@gmail.com>
2019-08-26 20:18:17 -0500

    Add a couple more utility functions

--- Makefile
+++ Makefile
@@ -1,22 +1,22 @@
-.PHONY: default docs release clean help test gh-pages
-
 default: help
 
+.PHONY: clean
 clean: ## Remove built docs and packaging artifacts
 	rm -rf dist build zgulde.egg-info public
 	rm -f index.html
 
-release: clean test gh-pages ## Release a new version to pypi
+.PHONY: release
+release: clean test lint gh-pages ## Release a new version to pypi
 	python3 setup.py sdist bdist_wheel
 	python3 -m twine upload dist/*
 
+.PHONY: docs gh-pages
 docs: ## Build the docs for extend_pandas
 	mkdir -p public
 	PYTHONPATH=. python doc/gen_extend_pandas_docs.py |\
 		rst2html.py --stylesheet-path=doc/style.css \
 		--template=doc/template.txt \
 		> public/index.html
-
 gh-pages: clean ## Build, commit, and push docs for the gh-pages branch
 	mkdir -p public
 	@if [[ ! -f .git/refs/heads/gh-pages ]] ; then \
@@ -34,9 +34,19 @@ gh-pages: clean ## Build, commit, and push docs for the gh-pages branch
 	git checkout --force master
 	git push origin gh-pages --force
 
+.PHONY: test
 test: ## Run the tests for zgulde/extend_pandas and zgulde/__init__
 	python -m doctest -o NORMALIZE_WHITESPACE -o ELLIPSIS zgulde/extend_pandas.py
 	python -m doctest -o NORMALIZE_WHITESPACE -o ELLIPSIS zgulde/__init__.py
 
+.PHONY: lint-pytype lint-mypy lint
+lint-pytype:
+	pytype zgulde/extend_pandas.py
+	pytype zgulde/__init__.py
+lint-mypy:
+	python -m mypy zgulde/__init__.py
+lint: lint-pytype lint-mypy ## Check types
+
+.PHONY: help
 help: ## Show this help message
 	@grep -E '^[a-zA-Z._-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%s\033[0m\t%s\n", $$1, $$2}' | column -ts$$'\t'
--- zgulde/__init__.py
+++ zgulde/__init__.py
@@ -1,4 +1,5 @@
-from typing import Callable, List, Iterable, Dict, Any
+from typing import Callable, List, Iterable, Dict, Any, TypeVar, Tuple, Optional, Sequence
+import itertools as it
 from functools import reduce, partial
 
 def pluck(d: Dict, *ks):
@@ -47,7 +48,7 @@ def comp(*fns: Iterable[Callable]):
     '''
     return partial(reduce, lambda x, f: f(x), reversed(fns))
 
-def partition(xs: Iterable, chunksize: int):
+def partition(xs: Sequence, chunksize: int):
     '''
     Partition a sequence into smaller subsequences
 
@@ -66,7 +67,7 @@ def partition(xs: Iterable, chunksize: int):
 
 chunk = partition
 
-def pipe(v: Any, *fns: List[Callable]):
+def pipe(v: Any, *fns: Callable):
     '''
     Thread a value through one or more functions.
 
@@ -85,3 +86,60 @@ def pipe(v: Any, *fns: List[Callable]):
     '''
 
     return reduce(lambda x, f: f(x), fns, v)
+
+A = TypeVar('A')
+def and_prev(xs: Iterable[A]) -> Iterable[Tuple[Optional[A], A]]:
+    '''
+    Return each item of the iterable xs, along with the previous item.
+
+    The first previous item is None.
+
+    Returns
+    -------
+
+    An iterable of tuples, where each tuple is the previous item and the current
+    item.
+
+    >>> list(and_prev([1, 2, 3]))
+    [(None, 1), (1, 2), (2, 3)]
+    '''
+    return zip(it.chain([None], xs), xs)
+
+def and_next(xs: Iterable[A]) -> Iterable[Tuple[A, Optional[A]]]:
+    '''
+    Return each of the items in the iterable xs, along with the next item.
+
+    When the iterable is exhausted, the last item is None.
+
+    Returns
+    -------
+
+    An iterable of tuples, where each tuple is the current item and the next
+    item.
+
+    >>> list(and_next([1, 2, 3]))
+    [(1, 2), (2, 3), (3, None)]
+    '''
+    return it.zip_longest(xs, it.islice(xs, 1, None))
+
+def prev_and_next(xs: Iterable[A]) -> Iterable[Tuple[Optional[A], A, Optional[A]]]:
+    '''
+    Return each of the items in the iterable xs, along with the previous and
+    next elements.
+
+    For the first item, the previous is None, for the last item, the next is
+    None.
+
+    Returns
+    -------
+
+    An iterable of tuples, where each tuple is (prev, current, next)
+
+    >>> list(prev_and_next([1, 2, 3]))
+    [(None, 1, 2), (1, 2, 3), (2, 3, None)]
+    '''
+    prev, current, next = it.tee(xs, 3)
+    return zip(it.chain([None], prev),
+               current,
+               it.chain(it.islice(next, 1, None), [None]))
+
