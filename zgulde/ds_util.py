0faf2ba5ea605f8ceec775571b523365f46e39a8
Zach Gulde <zachgulde@gmail.com>
2019-11-25 08:33:59 -0600

    Add more util methods

--- zgulde/ds_util.py
+++ zgulde/ds_util.py
@@ -63,3 +63,102 @@ def rand_dates(start, end, n):
     start_u = pd.to_datetime(start).value // 10**9
     end_u = pd.to_datetime(end).value // 10**9
     return pd.DatetimeIndex((10**9*np.random.randint(start_u, end_u, n)).view('M8[ns]'))
+
+import graphviz
+from sklearn.tree import export_graphviz, DecisionTreeClassifier
+
+def viz_dtree(X, y, **kwargs):
+    tree = DecisionTreeClassifier(**kwargs).fit(X, y)
+    feature_names = X.columns
+    class_names = sorted(y.unique())
+    dot = export_graphviz(
+        tree,
+        out_file=None,
+        feature_names=feature_names,
+        class_names=class_names, # target value names
+        special_characters=True,
+        filled=True,             # fill nodes w/ informative colors
+        impurity=False,          # show impurity at each node
+        leaves_parallel=True,    # all leaves at the bottom
+        proportion=True,         # show percentages instead of numbers at each leaf
+        rotate=True,             # left to right instead of top-bottom
+        rounded=True,            # rounded boxes and sans-serif font
+    )
+    graph = graphviz.Source(dot, filename='dtree', format='png')
+    graph.view(cleanup=True)
+
+import math
+import numpy as np
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+def tree(df, rformula, **kwargs):
+    X, y = df.rformula(rformula)
+    if np.issubdtype(y.dtype, np.number):
+        return dtree_regressor(X, y, **kwargs)
+    else:
+        y = y.astype('category').cat.codes
+        return dtree_classifier(X, y, **kwargs)
+
+def lr(df, rformula, **kwargs):
+    X, y = df.rformula(rformula)
+    if np.issubdtype(y.dtype, np.number):
+        return linear_regression(X, y, **kwargs)
+    else:
+        y = y.astype('category').cat.codes
+        return logistic_regression(X, y, **kwargs)
+
+def logistic_regression(X, y, **kwargs):
+    lr = LogisticRegression(**kwargs).fit(X, y)
+    yhat = lr.predict(X)
+    coefs = pd.Series(dict(zip(X.columns, lr.coef_[0])))
+    return {
+        'model': lr,
+        'accuracy': accuracy_score(y, yhat),
+        'precision': precision_score(y, yhat, average=None),
+        'recall': recall_score(y, yhat, average=None),
+        'coef': coefs.sort_values()
+    }
+
+def dtree_classifier(X, y, **kwargs):
+    tree = DecisionTreeClassifier(**kwargs).fit(X, y)
+    yhat = tree.predict(X)
+    features = pd.Series(dict(zip(X.columns, tree.feature_importances_)))
+    return {
+        'model': tree,
+        'accuracy': accuracy_score(y, yhat),
+        'precision': precision_score(y, yhat, average=None),
+        'recall': recall_score(y, yhat, average=None),
+        'feature_importances': features.sort_values()
+    }
+
+def dtree_regressor(X, y, **kwargs):
+    tree = DecisionTreeRegressor(**kwargs).fit(X, y)
+    yhat = tree.predict(X)
+    mse = mean_squared_error(y, yhat)
+    features = pd.Series(dict(zip(X.columns, tree.feature_importances_)))
+    return {
+        'model': tree,
+        'r2': r2_score(y, yhat),
+        'mse': mse,
+        'rmse': math.sqrt(mse),
+        'mae': mean_absolute_error(y, yhat),
+        'feature_importances': features.sort_values(),
+    }
+
+def linear_regression(X, y, **kwargs):
+    lr = LinearRegression(**kwargs).fit(X, y)
+    yhat = lr.predict(X)
+    mse = mean_squared_error(y, yhat)
+    coefs = pd.Series(dict(zip(X.columns, lr.coef_)))
+    return {
+        'model': lr,
+        'r2': r2_score(y, yhat),
+        'mse': mse,
+        'rmse': math.sqrt(mse),
+        'mae': mean_absolute_error(y, yhat),
+        'coef': coefs.sort_values(),
+    }
