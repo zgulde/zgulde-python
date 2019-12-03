707a46c20da553fb49882d8af1e01410141759b8
Zach Gulde <zachgulde@gmail.com>
2019-12-03 07:54:20 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='zgulde',
-    version='0.0.33',
+    version='0.0.34',
     author='Zach Gulde',
     author_email='zachgulde@gmail.com',
     description='A Small Person Utility Library',
