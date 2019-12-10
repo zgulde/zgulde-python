30b7f36faae2466772b262e57ac56bf7411a3cca
Zach Gulde <zachgulde@gmail.com>
2019-12-10 15:47:30 -0600

    Add NLP utility functions

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='zgulde',
-    version='0.0.38',
-    author='Zach Gulde',
-    author_email='zachgulde@gmail.com',
-    description='A Small Person Utility Library',
+    name="zgulde",
+    version="0.0.38",
+    author="Zach Gulde",
+    author_email="zachgulde@gmail.com",
+    description="A Small Person Utility Library",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/zgulde/zgulde-python',
+    long_description_content_type="text/markdown",
+    url="https://github.com/zgulde/zgulde-python",
     packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
--- zgulde/ds_util/nlp.py
+++ zgulde/ds_util/nlp.py
@@ -1,12 +1,44 @@
-from typing import Iterable
+from typing import Iterable, Union, Tuple
 
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from toolz import comp, partial
 
 
-def bag_of_words(documents: Iterable[str]) -> pd.DataFrame:
+def tfidfs(
+    documents: Iterable[str], return_vectorizer=False
+) -> Union[
+    pd.DataFrame, Tuple[pd.DataFrame, sklearn.feature_extraction.text.TfidfVectorizer]
+]:
     """
-    >>> docs = ['first article', 'second article', 'mary had a little lamb, little lamb']
+    >>> docs = [
+    ...     "doc number one",
+    ...     "this is the second one",
+    ...     "this is the third doc"
+    ... ]
+    >>> tfidfs(docs)
+            doc        is    number       one    second       the     third      this
+    0  0.517856  0.000000  0.680919  0.517856  0.000000  0.000000  0.000000  0.000000
+    1  0.000000  0.417796  0.000000  0.417796  0.549351  0.417796  0.000000  0.417796
+    2  0.417796  0.417796  0.000000  0.000000  0.000000  0.417796  0.549351  0.417796
+    """
+    vectorizer = TfidfVectorizer()
+    m = vectorizer.fit_transform(documents).todense()
+    df = pd.DataFrame(m, columns=vectorizer.get_feature_names())
+    if return_vectorizer:
+        return df, vectorizer
+    else:
+        return df
+
+
+def bag_of_words(
+    documents: Iterable[str], return_cv=False
+) -> Union[
+    pd.DataFrame, Tuple[pd.DataFrame, sklearn.feature_extraction.text.CountVectorizer]
+]:
+    """
+    >>> docs = ["first article", "second article", "mary had a little lamb, little lamb"]
     >>> bag_of_words(docs)
        article  first  had  lamb  little  mary  second
     0        1      1    0     0       0     0       0
@@ -15,4 +47,17 @@ def bag_of_words(documents: Iterable[str]) -> pd.DataFrame:
     """
     cv = CountVectorizer()
     m = cv.fit_transform(documents).todense()
-    return pd.DataFrame(m, columns=cv.get_feature_names())
+    bow = pd.DataFrame(m, columns=cv.get_feature_names())
+    if return_cv:
+        return bow, cv
+    else:
+        return bow
+
+
+def top_n_ngrams(s: pd.Series, top_n=3, ngrams=3):
+    """
+    Extract the top_n number of ngrams (n=ngrams) from s. s is assumed to be a
+    pandas Series full of text data.
+    """
+    f = comp(pd.Series, partial(nltk.ngrams, n=ngrams), str.split)
+    return s.apply(f).stack().value_counts().head(top_n)
