93dfb6db3660ac4d864c970a157d32fc47a3a4c8
Zach Gulde <zachgulde@gmail.com>
2020-01-22 14:06:11 -0600

    Tweak plotting api

--- zgulde/ds_util/plotting.py
+++ zgulde/ds_util/plotting.py
@@ -7,13 +7,15 @@ import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 from pydataset import data
 
+import zgulde.extend_pandas
+
 # TODO: refactor some of these to iterate over .groupby instead of unique values
 
 
-def plot_dual_axis(df: pd.DataFrame, x: str) -> Callable:
+def dual_axis(df: pd.DataFrame, x: str) -> Callable:
     """
     >>> mpg = data("mpg")
-    >>> plot_dual_axis(mpg, "displ")("hwy")("cty")
+    >>> dual_axis(mpg, "displ")("hwy")("cty")
     <Figure size ... with 2 Axes>
     """
     fig, ax1 = plt.subplots()
@@ -30,15 +32,12 @@ def plot_dual_axis(df: pd.DataFrame, x: str) -> Callable:
     return plot_y1
 
 
-def plot_bar_dual_y(
+def bar_dual_y(
     df: pd.DataFrame, x: str, y1: str, y2: str, aggfunc="mean", space=0.1, ax=None
 ):
     g = df[[x, y1, y2]].groupby(x).agg(aggfunc)
 
-    if ax is None:
-        fig, ax1 = plt.subplots()
-    else:
-        fig, ax1 = None, ax
+    fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
 
     ax1_xticks = [x - (0.5 - space) for x in range(len(g.index))]
@@ -65,15 +64,10 @@ def plot_3d(df, x, y, z, g):
     ax.legend(title=g)
 
 
-def top_n(s: pd.Series, n=3, other_val="Other"):
-    top_n = s.value_counts().index[:n]
-    return pd.Series(np.where(s.isin(top_n), s, other_val), name=s.name)
-
-
-def plot_hist_by_group(x: pd.Series, g: pd.Series, *args, **kwargs):
+def hist_by_group(x: pd.Series, g: pd.Series, *args, **kwargs):
     """
     >>> mpg = data('mpg')
-    >>> plot_hist_by_group(mpg.hwy, mpg.cyl)
+    >>> hist_by_group(mpg.hwy, mpg.cyl)
     (<Figure size ... with 4 Axes>, array([[<matplotlib.axes._subplots.AxesSubplot object at ...>,
             <matplotlib.axes._subplots.AxesSubplot object at ...>],
            [<matplotlib.axes._subplots.AxesSubplot object at ...>,
@@ -82,7 +76,7 @@ def plot_hist_by_group(x: pd.Series, g: pd.Series, *args, **kwargs):
     """
     fig, axs = plt.subplots(2, 2) if g.nunique() > 2 else plt.subplots(1, 2)
     fig.suptitle(f"Distribution of {x.name} by {g.name}")
-    g = top_n(g, 3)
+    g = g.top_n(3)
     g.index = x.index
 
     for ax, v in zip(axs.ravel(), g.unique()):
@@ -104,13 +98,13 @@ def plot_hist_by_group(x: pd.Series, g: pd.Series, *args, **kwargs):
     return fig, axs
 
 
-def plot_bar_by_group(x: pd.Series, g: pd.Series, aggfunc="mean", *args, **kwargs):
+def bar_by_group(x: pd.Series, g: pd.Series, aggfunc="mean", *args, **kwargs):
     """
     >>> mpg = data('mpg')
-    >>> plot_bar_by_group(mpg.hwy, mpg['class'])
+    >>> bar_by_group(mpg.hwy, mpg['class'])
     (<Figure size ... with 1 Axes>, <matplotlib.axes._subplots.AxesSubplot object at ...>)
     """
-    g = top_n(g, 3)
+    g = g.top_n(3)
     fig, ax = plt.subplots()
     x.groupby(g).agg(aggfunc).plot.bar(ax=ax, color="pink", width=1)
     ax.set(title=f"{aggfunc} of {x.name} by {g.name}")
@@ -126,7 +120,7 @@ def plot_bar_by_group(x: pd.Series, g: pd.Series, aggfunc="mean", *args, **kwarg
     return fig, ax
 
 
-def plot_scatter_by_group(df: pd.DataFrame, x: str, y: str, g: str):
+def scatter_by_group(df: pd.DataFrame, x: str, y: str, g: str):
     for name, subset in df.groupby(g):
         plt.scatter(subset[x], subset[y], label=name)
     plt.xlabel(x)
@@ -151,11 +145,11 @@ arrowstyles = [
 ]
 
 
-def annotate(s, xy, xytext, style="->"):
+def _annotate(s, xy, xytext, style="->"):
     return plt.annotate(s, xy=xy, xytext=xytext, arrowprops={"arrowstyle": style})
 
 
-def arrow(fromxy, toxy, style="->"):
+def _arrow(fromxy, toxy, style="->"):
     plt.annotate(
         "",
         xy=(11, 0.07),
