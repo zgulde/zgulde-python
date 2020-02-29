ba0d549584f907e4e11bf25288432d17413dfe3a
Zach Gulde <zachgulde@gmail.com>
2020-02-29 14:43:05 -0600

    Extract slide display function

--- zgulde/nbslides/__main__.py
+++ zgulde/nbslides/__main__.py
@@ -2,6 +2,8 @@ import json
 import os
 import sys
 
+from zgulde import slurp
+
 
 def display_markdown(cell, output_slide_seperator=True, classes=None):
     if output_slide_seperator:
@@ -11,18 +13,20 @@ def display_markdown(cell, output_slide_seperator=True, classes=None):
     print("".join(cell["source"]))
 
 
-def display_code(cell, show_source=True, show_execute_results=True):
-    # TODO: display cell["execution_count"]
+def display_code(
+    cell, show_source=True, show_execute_results=True, show_html_results=True
+):
+    # TODO: display cell["execution_count"]?
     print("")
     if show_source:
         print("```python")
         print("".join(cell["source"]))
         print("```")
     for output in cell["outputs"]:
-        if output["output_type"] == "execute_result" and show_execute_results:
+        if output["output_type"] == "execute_result":
             if "text/html" in output["data"]:
                 print("".join(output["data"]["text/html"]))
-            else:
+            elif show_execute_results:
                 print("```")
                 print("".join(output["data"]["text/plain"]))
                 print("```")
@@ -34,9 +38,31 @@ def display_code(cell, show_source=True, show_execute_results=True):
                 )
             )
         else:
-            # TODO: raise?
-            pass
+            raise Exception("Unknown output_type, %s" % output["output_type"])
     print("")
+
+
+def display_notebook(notebook):
+    # we assume the first cell is the title slide
+    title_slide = notebook["cells"][0]
+    display_markdown(
+        title_slide,
+        output_slide_seperator=False,
+        classes=["center", "middle", "text-invert"],
+    )
+
+    for cell in notebook["cells"][1:]:
+        if cell["cell_type"] == "markdown":
+            display_markdown(cell)
+        elif cell["cell_type"] == "code":
+            display_code(
+                cell,
+                show_source=args.show_source,
+                show_execute_results=args.show_execute_results,
+            )
+        else:
+            # TODO: handle raw data type
+            pass
 
 
 if __name__ == "__main__":
@@ -62,7 +88,7 @@ if __name__ == "__main__":
         "--html-boilerplate",
         action="store_true",
         default=False,
-        help="Include html boilerplate in output. default: $(default)s",
+        help="Include html boilerplate in output. default: %(default)s",
     )
     parser.add_argument("notebook", help="path to the jupyter notebook file")
 
@@ -75,30 +101,14 @@ if __name__ == "__main__":
     notebook = json.load(open(args.notebook))
 
     if args.html_boilerplate:
-        print(
-            '<html><head><meta charset="utf-8"/><title>My Presentation</title></head><body><textarea id="source">'
-        )
-
-    # we assume the first cell is the title slide
-    title_slide = notebook["cells"][0]
-    display_markdown(
-        title_slide,
-        output_slide_seperator=False,
-        classes=["center", "middle", "text-invert"],
-    )
-
-    for cell in notebook["cells"][1:]:
-        if cell["cell_type"] == "markdown":
-            display_markdown(cell)
-        elif cell["cell_type"] == "code":
-            display_code(
-                cell,
-                show_source=args.show_source,
-                show_execute_results=args.show_execute_results,
-            )
-        else:
-            # TODO: handle raw data type
-            pass
+        print("<html><head>")
+        print('<meta charset="utf-8"/><title>My Presentation</title>')
+        print("<style>")
+        print(slurp(os.path.dirname(__file__) + "/style.css"))
+        print("</style>")
+        print("</head><body>")
+        print('<textarea id="source">')
+    display_notebook(notebook)
 
     if args.html_boilerplate:
         print(
