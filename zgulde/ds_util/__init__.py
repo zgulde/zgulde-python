183a68bd4cb333cfdada907073e64d6899bc74fa
Zach Gulde <zachgulde@gmail.com>
2022-02-24 14:44:23 -0600

    Add csv dataframe caching decorator

--- zgulde/ds_util/__init__.py
+++ zgulde/ds_util/__init__.py
@@ -9,6 +9,55 @@ from zgulde.ds_util.modeling import *
 # mode aggregation
 # tips.groupby('size').agg(stats.mode).apply(lambda s: s.apply(lambda v: v[0][0]))
 
+def with_caching(filename='data.csv', index=False, with_prints=True):
+    '''
+    Decorator for wrapping any function that returns a dataframe so that it
+    saves the dataframe to a local file and on subsequent calls reads from the
+    local file. Useful for data acquisition that can take a while.
+
+    Parameters
+    ----------
+    - filename: filename for the cache file, e.g. data.csv
+    - index: whether or not to include the dataframe index
+    - with_prints: booleean indicating whether to print information about
+      whether the data is coming from the original function or the cache file
+
+    Examples
+    --------
+
+    >>> @with_caching('demo.csv')
+    ... def get_some_data():
+    ...     return pd.DataFrame(dict(x=[1, 2, 3], y=list('abc')))
+    >>> os.unlink('demo.csv') # remove the cache file if it exists
+    >>> get_some_data()
+    Reading data from get_some_data
+       x  y
+    0  1  a
+    1  2  b
+    2  3  c
+    >>> get_some_data()
+    Reading from cached csv file
+       x  y
+    0  1  a
+    1  2  b
+    2  3  c
+    '''
+    def wrapper(fn):
+        @wraps(fn)
+        def f(*args, **kwargs):
+            if os.path.exists(filename):
+                if with_prints:
+                    print('Reading from cached csv file')
+                return pd.read_csv(filename)
+            if with_prints:
+                print(f'Reading data from {fn.__name__}')
+            df = fn(*args, **kwargs)
+            df.to_csv(filename, index=index)
+            return df
+        return f
+    return wrapper
+
+
 # better interaction w/ sklearn.metrics.confusion_matrix, e.g. auto labelling
 def better_confusion_matrix(actual, predicted, labels=None):
     cm = pd.crosstab(actual, predicted)
